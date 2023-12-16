dd0eae66079861d92bf81136b56b30d01defb58f
gothictomato <106775154+gothictomato@users.noreply.github.com>
2023-12-16 02:42:25 -0500

    [gothictomato]: Initial commit

---
+++ src/main/java/io/gothictomato/mobai/MobAI.java
@@ -0,0 +1,30 @@
+package io.gothictomato.mobai;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = MobAI.MODID, name = MobAI.NAME, version = MobAI.VERSION)
+public class MobAI {
+    public static final String MODID = "mobai";
+    public static final String NAME = "MobAI";
+    public static final String VERSION = "0.0.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
